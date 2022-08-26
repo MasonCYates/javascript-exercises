ecf5cffd7c7e6c74cbbd73df3b85c5ade7c30826
Mcyates <mcyatesvm@gmail.com>
2022-08-26 04:48:55 -0500

    finished these challenges

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
 
-};
+function repeatString(string,times) {
+    let repeat = "";
 
+    while(times > 0) {
+        repeat += string;
+        times--;
+    }
+    if(times < 0){
+        return "ERROR";
+    }
+    if()
+   return repeat;
+}
+repeatString();
 // Do not edit below this line
 module.exports = repeatString;
